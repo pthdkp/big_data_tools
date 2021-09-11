c854702617b5b4ba9cca6ba7cf6cc76c6e885526
pthdkp <90509293+pthdkp@users.noreply.github.com>
2021-09-11 20:06:13 +0700

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import datetime
+for line in sys.stdin:
+	# remove leading and trailing whitespace
+	line = line.strip()
+	rows = line.split(",")
+	(name1,phone1,name2,phone2,timestamp_ms,timestamp_str,duration_m) = tuple(rows)
+	#print(rows)
+	# write the results to STDOUT (standard output);
+	# what we output here will be the input for the
+	# Reduce step, i.e. the input for reducer.py
+	try: 
+		print('{}\t{}'.format(datetime.datetime.strptime(TimeStamp_str, '%Y-%m-%d %H-%M-%S').strftime("%A"), int(duration_m)))
+	except ValueError as e:
+		continue
