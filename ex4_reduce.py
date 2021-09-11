f6ca5ebcd8b42aff6584e3cf116cab203fc98c61
pthdkp <90509293+pthdkp@users.noreply.github.com>
2021-09-11 20:08:03 +0700

    Add files via upload

---
+++ ex4_reduce.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from operator import itemgetter
+import sys
+
+current_key = None
+min = 0
+max = 0
+
+# input comes from STDIN
+for line in sys.stdin:
+	# remove leading and trailing whitespace
+	line = line.strip()
+	row = line.split("\t")
+	(key,value) = row
+
+	try: 
+		value = int(value)
+	except ValueError:
+		continue
+
+	# this IF-switch only works because Hadoop sorts map output
+	# by key (here: word) before it is passed to the reducer
+	if current_key == key:
+		if value < min:
+			min = value
+		if value > max:
+			max = value
+		else:
+			min = min
+			max = max
+	else:
+		if current_key:
+	 		# write result to STDOUT
+			print('{}\t{}'.format(current_key, min, max))
+		current_key = key
+		min = value
+		max = value
+
+#Last key
+if current_key == key:
+	print('{}\t{}'.format(current_key, min, max))
